289e10b2ed0b91c6dc0adb3446298c4e75bfbda1
Jackie Watanabe <sushiigirl@gmail.com>
2017-09-27 00:37:31 -0700

    added arrow function playground file

--- playground/arrow-function.js
+++ playground/arrow-function.js
@@ -1,6 +1,21 @@
-var square = x => x * x
+var square = x => x * x;
 // {
 //   var result = ;
 //
 // };
 console.log(square(9));
+
+var user = {
+  name: 'Jackie',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  }
+};
+
+user.sayHi(1, 2, 3);
+user.sayHiAlt(1, 2, 3);
