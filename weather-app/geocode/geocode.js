04ea26dc0be567ebea0671d7a11e6c477615dfb2
Jackie Watanabe <sushiigirl@gmail.com>
2017-09-30 15:22:53 -0700

    weather app

---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Google servers');
+      // console.log();
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log('No results found');
+    } else {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+      // console.log(`Address: ${body.results[0].formatted_address}`);
+      // console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      // console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+      // console.log(JSON.stringify(error, null, 2));
+    }
+  });
+}
+
+module.exports = {
+  geocodeAddress
+}
